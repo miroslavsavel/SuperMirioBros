70a2c67125646f6747c69fdc0c6c70e7edaf701b
Miroslav Savel <miroslav.savel@gmail.com>
2022-04-13 10:30:44 +0200

    add player to the level

--- level.py
+++ level.py
@@ -1,11 +1,14 @@
 import pygame
 from tiles import Tile
+from player import Player
 from settings import tile_size
 
 class Level:
     def __init__(self, level_data, surface_to_draw):
+        # level setup
         self.display_surface = surface_to_draw
         self.setup_level(level_data)
+        self.world_shift = 0
 
     def setup_level(self, layout):  #layout will be lvel_data
         self.tiles = pygame.sprite.Group()
@@ -16,8 +19,13 @@ class Level:
                     y = row_index * tile_size
                     tile = Tile((x,y),tile_size)    #position is tuple!
                     self.tiles.add(tile)
+                if cell == 'P':
+                    x = col_index * tile_size
+                    y = row_index * tile_size
+                    tile = Player((x, y))  # position is tuple!
 
     def run(self):
-        self.tiles.update(1)    # scroll entire level to the right
+        self.tiles.update(self.world_shift)    # scroll entire level to the right
         self.tiles.draw(self.display_surface)
+        self.player.draw(self.display_surface)
         pass
