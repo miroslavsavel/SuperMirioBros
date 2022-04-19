be70b5958b440482e70a7bc9654906ee0e2fc646
Bytesabre <miroslav.savel@gmail.com>
2022-04-19 20:52:24 +0200

    method for applying gravity on the player object

--- player.py
+++ player.py
@@ -6,8 +6,12 @@ class Player(pygame.sprite.Sprite):
         self.image = pygame.Surface((32,64))
         self.image.fill('red')
         self.rect = self.image.get_rect(topleft = pos)
+
+        # player movement
         self.direction = pygame.math.Vector2(0,0)
         self.speed = 8
+        self.gravity = 0.8
+        self.jump_speed = -16   # negative because I want to jump up
 
     def get_input(self):
         keys = pygame.key.get_pressed()
@@ -22,6 +26,11 @@ class Player(pygame.sprite.Sprite):
             self.direction.x = 0
             pass
 
+    def apply_gravity(self):
+        self.direction.y += self.gravity
+        self.rect.y += self.direction.y
+
     def update(self):
         self.get_input()
         self.rect.x += self.direction.x * self.speed
+        self.apply_gravity()
